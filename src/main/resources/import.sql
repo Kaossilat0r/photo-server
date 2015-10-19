839956e879d2cc3b7cc8c97148bb257b52d39f18
roman <kaossilat0r.rn@gmail.com>
2015-10-19 13:38:47 +0200

    updated import dataset

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,9 +2,18 @@ insert into user (id, name) values (1, 'Alice');
 insert into user (id, name) values (2, 'Bob');
 insert into user (id, name) values (3, 'Carl');
 
-insert into picture (fullurl, thumburl, user_id) values ('full1', 'thumb1', 1);
-insert into picture (fullurl, thumburl, user_id) values ('full2', 'thumb2', 1);
-insert into picture (fullurl, thumburl, user_id) values ('full3', 'thumb3', 1);
-insert into picture (fullurl, thumburl, user_id) values ('full4', 'thumb4', 2);
-insert into picture (fullurl, thumburl, user_id) values ('full5', 'thumb5', 2);
-insert into picture (fullurl, thumburl, user_id) values ('full6', 'thumb6', 2);
+
+insert into picture (fullurl, thumburl, user_id) values ('images/DSC_0194.JPG',            'images/DSC_0194_th.png'            , 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/DSC_0270.JPG',            'images/DSC_0270_th.png'            , 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/IMG_3797.JPG',            'images/IMG_3797_th.png'            , 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/IMG_3827.JPG',            'images/IMG_3827_th.png'            , 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/IMG_3842.JPG',            'images/IMG_3842_th.png'            , 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/IMG_3848.JPG',            'images/IMG_3848_th.png'            , 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/20141011_205644_5244.jpg','images/20141011_205644_5244_th.png', 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/20141011_205812_5245.jpg','images/20141011_205812_5245_th.png', 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/20141011_210304_5260.jpg','images/20141011_210304_5260_th.png', 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/20141011_213735_5309.jpg','images/20141011_213735_5309_th.png', 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/20141017_210214_6490.jpg','images/20141017_210214_6490_th.png', 1);
+insert into picture (fullurl, thumburl, user_id) values ('images/20141017_213111_6633.jpg','images/20141017_213111_6633_th.png', 2);
+insert into picture (fullurl, thumburl, user_id) values ('images/20141017_214013_6688.jpg','images/20141017_214013_6688_th.png', 2);
+insert into picture (fullurl, thumburl, user_id) values ('images/20141017_214112_6702.jpg','images/20141017_214112_6702_th.png', 2);
